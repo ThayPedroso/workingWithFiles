297f17a36ef60a2c35de3450872ecc6550e1f0e1
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-02-03 22:19:01 -0300

    general example with filestream

--- fileStream/Program.cs
+++ fileStream/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace fileStream
 {
@@ -6,7 +7,27 @@ namespace fileStream
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
         }
     }
 }
